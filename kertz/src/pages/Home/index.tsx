95f3d9031f032bcde1502ee28bdf50460a12b3b9
aleks <alexskntks@outlook.com>
2026-06-12 15:42:36 -0300

    adiciona a paleta de cores no root

--- kertz/src/pages/Home/index.tsx
+++ kertz/src/pages/Home/index.tsx
@@ -1,15 +1,35 @@
+import Features from "../../components/Features";
+import FinalCTA from "../../components/FinalCTA";
+import Hero from "../../components/Hero";
 import { Container } from "../../components/Layout/Container";
+import Pricing from "../../components/Pricing";
 import { MainLayout } from "../../layouts/MainLayout";
 
-//O container tem as propriedades
+// ATENÇÃO
+// Menu de navegação, NÃO fica na página, fica no Layout
+// Footer, NÃO fica na página, fica no Layout
+
+//Exemplo
+// <Container id="example" bgColor="red" padding="10">
+//   <h1>Isso é um titulo</h1>
+//   <p>Conteúdo principal do site</p>
+// </Container>
 
 // src/pages/Home/index.tsx
 export function Home() {
   return (
     <MainLayout>
-      <Container id="teste" bgColor="red" padding="10">
-        <h1>Isso é um titulo</h1>
-        <p>Conteúdo principal do site</p>
+      <Container>
+        <Hero />
+      </Container>
+      <Container>
+        <Features />
+      </Container>
+      <Container>
+        <Pricing />
+      </Container>
+      <Container>
+        <FinalCTA />
       </Container>
     </MainLayout>
   );
